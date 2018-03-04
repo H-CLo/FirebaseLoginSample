41310f52deaab9063715d9678961dc434f5f6d42
Hung Chang Lo <nba1895@gmail.com>
2018-03-04 13:22:06 +0800

    Add project

---
+++ FirebaseLoginSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirebaseLoginSample
+//
+//  Created by Hung Chang Lo on 2018/3/4.
+//  Copyright © 2018年 Hung Chang Lo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
